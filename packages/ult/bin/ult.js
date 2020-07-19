c79c038bf04851220bb097ff8631a17b7f5118ac
Cavitt <cavitt.glover@gmail.com>
2020-07-19 14:48:36 -0500

    Project tool refactoring, auto pod install

--- packages/ult/bin/ult.js
+++ packages/ult/bin/ult.js
@@ -12,6 +12,7 @@ const cwd = path.resolve(process.cwd(), name.toLowerCase());
 const tpl = `ult-template-${flag ? flag.substr(2) : 'default'}`;
 
 async function init() {
+  // Validation
   if (!name)
     return console.log(chalk.red('Project name is missing. (e.g. npx ult Demo)'));
   if (name.match(/^[_\.]/))
@@ -20,6 +21,7 @@ async function init() {
     return console.log(chalk.red('Project name should not contain: ~ ( ) \' ! *'));
   if (name.length > 100) 
     return console.log(chalk.red('Project name length cannot exceed 100 characters.'));
+  // Installation
   try {
     console.log('Creating project, please wait...');
     await utils.npx(['--ignore-existing', 'react-native', 'init', name, '--template', tpl]);
@@ -29,7 +31,11 @@ async function init() {
     await utils.npx(['react-native-macos-init'], cwd);
     console.log('Patching project files...\n');
     await utils.patch(name);
-    // TODO: install pods if on macos
+    if (process.platform === 'darwin') {
+      console.log('Installing pods...\n');
+      await utils.pod(name);
+    }
+    // Information
     console.log(chalk.green(`Successfully created ${name}!\n`));
     console.log(chalk.bold('1) Navigate to your project:'));
     console.log(`$ ${chalk.yellow(`cd ${name.toLowerCase()}`)}\n`);
@@ -40,6 +46,7 @@ async function init() {
     console.log(`$ ${chalk.yellow('npm run windows')}`);
     console.log(`$ ${chalk.yellow('npm run android')}`);
     console.log(chalk.blue('\nFor more details, visit https://docs.ult.dev\n'));
+  // Exception
   } catch (e) {
     console.log(chalk.red(`Failed to create project. (${e})`));
   }
--- packages/ult/lib/utils.js
+++ packages/ult/lib/utils.js
@@ -1,37 +1,42 @@
-const spawn = require('child_process').spawn;
 const path = require('path');
 const replace = require('replace-in-file');
+const spawn = require('child_process').spawn;
+const event = (a,b) => e => e === 0 ? a() : b(e);
 
+// Helper to run the "npx" cli tool
 function npx(args, cwd) {
-  return new Promise((resolve, reject) => {
-    spawn(process.platform === 'win32'
-      ? 'npx.cmd'
-      : 'npx', args, {cwd})
-      .once('exit', e => (e === 0
-        ? resolve()
-        : reject(e))
-      );
+  const cmd = process.platform === 'win32' ? 'npx.cmd' : 'npx';
+  return new Promise((res, rej) => {
+    spawn(cmd, args, {cwd}).once('exit', event(res, rej));
   });
 };
 
+// Helper to run the "pod" cli tool
+function pod(name) {
+  const cwd = path.resolve(process.cwd(), name.toLowerCase(), 'ios');
+  return new Promise((res, rej) => {
+    spawn('pod', ['install'], {cwd}).once('exit', event(res, rej));
+  });
+};
+
+// Patches Windows & MacOS main component names
 async function patch(name) {
-  const base = path.resolve(process.cwd(), name.toLowerCase());
+  const cwd = path.resolve(process.cwd(), name.toLowerCase());
   await replace({
     from: `MainComponentName(L"${name}");`,
     to: 'MainComponentName(L"RXApp");',
     files: [
-      `${base}/windows/${name}/App.cpp`,
+      `${cwd}/windows/${name}/App.cpp`,
     ],
   });
   await replace({
     from: `moduleName:@"${name}"`,
     to: 'moduleName:@"RXApp"',
     files: [
-      `${base}/macos/${name}-iOS/AppDelegate.m`,
-      `${base}/macos/${name}-macOS/ViewController.m`,
+      `${cwd}/macos/${name}-iOS/AppDelegate.m`,
+      `${cwd}/macos/${name}-macOS/ViewController.m`,
     ],
   });
 };
 
-
-module.exports = {npx, patch};
+module.exports = {npx, pod, patch};
