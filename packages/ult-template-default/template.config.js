db5584ab69c122baaf87683ac0ef1e66dc5d893a
Cavitt <cavitt.glover@gmail.com>
2021-04-04 20:23:03 -0500

    Update RN templates

--- packages/ult-template-default/template.config.js
+++ packages/ult-template-default/template.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  placeholderName: 'AppTemplate',
+  placeholderName: 'HelloWorld',
   templateDir: './template',
 }
--- packages/ult-template-default/template/ios/HelloWorldTests/HelloWorldTests.m
+++ packages/ult-template-default/template/ios/HelloWorldTests/HelloWorldTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AppTemplateTests : XCTestCase
+@interface HelloWorldTests : XCTestCase
 
 @end
 
-@implementation AppTemplateTests
+@implementation HelloWorldTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
