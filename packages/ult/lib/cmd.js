55416bc477dc614f62818041d2b3c3e4829cf61a
Cavitt <cavitt.glover@gmail.com>
2021-08-23 19:13:39 -0500

    Refactoring

--- packages/ult/lib/cmd.js
+++ packages/ult/lib/cmd.js
@@ -1,29 +1,24 @@
 const path = require('path');
 const spawn = require('child_process').spawn;
-const exit = (c,s,f) => e => e === 0 ? s() : f(`${c}: code ${e}`);
 
 // Run the "npx" cli tool
 function npx(args, cwd, inherit) {
-  const program = process.platform === 'win32' ? 'npx.cmd' : 'npx';
-  const command = `npx ${args[0]}`;
-  return cmd(program, command, cwd, inherit);
+  const cmd = process.platform === 'win32' ? 'npx.cmd' : 'npx';
+  return run(cmd, args, cwd, inherit);
 };
 
 // Run the "pod" cli tool
 function pod(project) {
-  const program = 'pod';
-  const command = command;
   const cwd = path.resolve(process.cwd(), project.toLowerCase(), 'ios');
-  return cmd(program, command, cwd);
+  return run('pod', ['install'], cwd);
 };
 
 // Run any command
-function cmd(program, command, directory, inherit) {
-  return new Promise((res, rej) => {
-    spawn(program, ['install'], {
-      cwd: directory,
-      stdio: inherit ? 'inherit' : undefined,
-    }).once('exit', exit(command, res, rej));
+function run(cmd, args, cwd, inherit) {
+  return new Promise((resolve, reject) => {
+    const options = {cwd, stdio: inherit ? 'inherit' : undefined};
+    spawn(cmd, args, options).once('exit', code =>
+      code === 0 ? resolve() : reject(`${cmd} exited with: ${code}`));
   });
 }
 
