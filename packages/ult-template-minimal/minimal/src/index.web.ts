f95d1ffaa60ae15407cc77aa38cc6199a2aa2680
Syntax <2079305+TheUltDev@users.noreply.github.com>
2023-12-21 20:04:18 -0600

    Remove template

--- packages/ult-template-minimal/minimal/src/index.web.ts
+++
@@ -1,7 +0,0 @@
-import {AppRegistry} from 'react-native';
-import {App} from './App';
-
-AppRegistry.registerComponent('HelloWorld', () => App);
-AppRegistry.runApplication('HelloWorld', {
-  rootTag: document.getElementById('root'),
-});
--- packages/ult-template-minimal/template.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  templateDir: './minimal',
-  placeholderName: 'HelloWorld',
-  titlePlaceholder: 'Hello App Display Name',
-}
